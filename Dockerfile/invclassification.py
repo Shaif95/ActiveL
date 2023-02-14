8a6c001c043258811d36a507996ed514af4d8e74
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-02-14 13:43:29 -0600

    Dockerfile

---
+++ Dockerfile/invclassification.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#from google.colab import files
+#uploaded = files.upload()
+
+
+# In[ ]:
+
+
+#!unzip data.zip
+
+
+# In[ ]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import os
+from sklearn.model_selection import train_test_split
+import json
+import glob
+import random
+import collections
+from glob import glob
+import numpy as np
+import pandas as pd
+import cv2
+import matplotlib.pyplot as plt
+import seaborn as sns
+import shutil
+import keras
+
+
+import os
+
+print("Path at terminal when executing this file")
+print(os.getcwd() + "\n")
+
+
+
